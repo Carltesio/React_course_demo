00d10a7e1f2e4033b376e502213bfbcd1a9f524a
Carlos <carlos.delgado@hotmail.it>
2020-05-11 19:45:12 +0200

    substituting hooks to pass props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
+const App = props => {
 
-  state = {
+  const [personState, setPersonState] = useState({
     persons: [
-      {name: 'Carlos', age: 33},
-      {name: 'Ida', age: 29}
+      { name: 'Carlos', age: 33 },
+      { name: 'Ida', age: 29 }
     ]
+  })
+
+  const switchNameHandler = () => {
+    setPersonState({
+      persons: [
+        { name: 'Carlos', age: 35 },
+        { name: 'Ida', age: 31 }
+      ]
+    })
   }
 
-  switchNameHandler = () => {
-    console.log('was clicked')
-  }
-
-  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-       </div> 
+        <button onClick={switchNameHandler}>Change name and age</button>
+        <Person 
+          name={personState.persons[0].name} age={personState.persons[0].age} />
+        <Person 
+          name={personState.persons[1].name} age={personState.persons[1].age} />
+      </div>
     )
-  }
 }
-
-export default App;
+export default App
