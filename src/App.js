9b5648efc1f724ca408116e205c99ce2f3ba4e16
Carlos <carlos.delgado@hotmail.it>
2020-05-11 21:24:41 +0200

    adding onChange events with hook useState

--- src/App.js
+++ src/App.js
@@ -20,15 +20,28 @@ const App = props => {
     })
   }
 
+  const nameChangeHandler = ( event ) => {
+    setPersonState({
+      persons: [
+        { name: 'Carlos', age: 35 },
+        { name: event.target.value, age: 31 }
+      ]
+    })
+  }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button onClick={switchNameHandler}>Change name and age</button>
         <Person 
-          name={personState.persons[0].name} age={personState.persons[0].age} />
+          name={personState.persons[0].name} 
+          age={personState.persons[0].age}
+          click={switchNameHandler} />
         <Person 
-          name={personState.persons[1].name} age={personState.persons[1].age} />
+          name={personState.persons[1].name} 
+          age={personState.persons[1].age}
+          changed= {nameChangeHandler} />
       </div>
     )
 }
