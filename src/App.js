b69c60a8180bd749575362b487bcefbe0b2b772d
Carlos <carlos.delgado@hotmail.it>
2020-05-14 18:46:21 +0200

    adding unique key to the person object, rearraging the component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ const App = props => {
 
   const [personState, setPersonState] = useState({
     persons: [
-      { name: 'Carlos', age: 33 },
-      { name: 'Ida', age: 29 }
+      {id: 1, name: 'Carlos', age: 33 },
+      {id: 2, name: 'Ida', age: 29 }
     ],
   })
 
   const [showPersonsState, setShowPersonsState] = useState({
-    showPersons: false
+    showPersons: true
   })
 
   const switchNameHandler = () => {
@@ -27,39 +27,57 @@ const App = props => {
   const nameChangeHandler = (event) => {
     setPersonState({
       persons: [
-        { name: 'Carlos', age: 35 },
+        { name: event.target.value, age: 35 },
         { name: event.target.value, age: 31 }
       ]
     })
   }
 
-  const togglePersonhandler = () => {
-    // const doesShow = showPersonsState
-    setShowPersonsState({
-      showPersons: !showPersonsState.showPersons
-    })
+  // const togglePersonhandler = () => {
+  //   const doesShow = showPersonsState.showPersons
+  //   setShowPersonsState({
+  //     showPersons: !doesShow
+  //   })
+  // }
+
+const deletePersonHandler = (index) => { 
+  const persons = [...personState.persons];
+  persons.splice(index, 1); 
+  setPersonState({persons: persons})
+}
+
+  let person = null;
+
+  if (showPersonsState.showPersons) {
+    person = (
+      < div >
+      {personState.persons.map((element, index) => { 
+        return <Person
+        click={ () =>deletePersonHandler(index)} 
+        name={element.name} 
+        age={element.age}
+        key= {element.id}
+        changed={nameChangeHandler}
+  />})}
+        {/* <Person
+          name={personState.persons[0].name}
+          age={personState.persons[0].age}
+          click={switchNameHandler} />
+        <Person
+          name={personState.persons[1].name}
+          age={personState.persons[1].age}
+          changed={nameChangeHandler} /> */}
+      </div>
+    )
+
   }
-
-
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
       <button className="button" id="button"
-        onClick={togglePersonhandler}>Change name and age</button>
-      {
-        showPersonsState.showPersons === true ?
-          < div >
-            <Person
-              name={personState.persons[0].name}
-              age={personState.persons[0].age}
-              click={switchNameHandler} />
-            <Person
-              name={personState.persons[1].name}
-              age={personState.persons[1].age}
-              changed={nameChangeHandler} />
-          </div> : null
-          }
+        onClick={nameChangeHandler}>Change name and age</button>
+      {person}
     </div >
   )
 }
